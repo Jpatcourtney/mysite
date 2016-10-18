f294c0afe8280ca53ad6c4e5df63517b6986a4f7
John Courtney <jpatcourtney@gmail.com>
2016-10-18 18:32:19 -0400

    added styling of portfolio site

--- jpatco/views.py
+++ jpatco/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def home(request):
+    return render(request, 'jpatco/home.html')
+
+def collection(request):
+    return render(request, 'jpatco/collection.html')
+
+def redirecthome(request):
+    return redirect('home')
